b1076963fa4ccfb397f32f397def7575fc75de72
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-02-10 00:49:58 +0300

    Cleaning

---
+++ biometric_auth_demoapp/lib/view/auth/view/auth_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthView extends StatelessWidget {
+  const AuthView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('AuthView'),
+      ),
+    );
+  }
+}
